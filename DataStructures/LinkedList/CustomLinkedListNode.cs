7095c0bcbf2b4e46b314bdd03836e114831f1d63
testtestovv.2023@gmail.com <testtestovv.2023@gmail.com>
2025-09-04 16:48:11 +0400

    Linked List searched

---
+++ DataStructures/LinkedList/CustomLinkedListNode.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace DataStructuresAndAlgorithms.DataStructures.LinkedList
+{
+    public class CustomLinkedListNode<T>
+    {
+        internal T? Element;
+        internal  CustomLinkedListNode<T>? Next { get; set; }
+
+        public CustomLinkedListNode()
+        {
+            
+        }
+        public CustomLinkedListNode(T element)
+        {
+            Element = element;
+        }
+
+       
+    }
+}
