119d28bfb265ac848bc13f6aa766e372b52277b4
testtestovv.2023@gmail.com <testtestovv.2023@gmail.com>
2025-09-08 13:38:58 +0400

    Stack with LinkedList implementation

---
+++ DataStructures/Stacks/LinkedListBaseStack/StackLinkedList.cs
@@ -0,0 +1,71 @@
+﻿using DataStructuresAndAlgorithms.DataStructures.LinkedLists.Linear;
+using System.Collections;
+
+namespace DataStructuresAndAlgorithms.DataStructures.Stacks.LinkedListBaseStack
+{
+    public class StackLinkedList<T> : IEnumerable, IEnumerable<T>
+    {
+        public int _size = 0;
+        public StackLinkedListNode<T>? _headNode;
+        public StackLinkedListNode<T>? _tailNode;
+        public StackLinkedList()
+        {
+            _headNode = null;
+            _tailNode = null;
+        }
+
+        public bool IsEmpty=> _size == 0;
+
+        public void Push(T item)
+        {
+            StackLinkedListNode<T> newNode=new StackLinkedListNode<T>(item);
+            if (IsEmpty)
+            {
+                _headNode = newNode;
+                _tailNode = newNode;
+            }
+            else
+            {
+               newNode.Next= _headNode;
+               _headNode = newNode;
+            }
+            _size++;
+        }
+
+        public T Pop()
+        {
+            if (IsEmpty)
+            {
+                throw new InvalidOperationException("Empty");
+            }
+            T lastElement = _headNode.Element;
+           _headNode= _headNode.Next;
+            _size--;
+            return lastElement;
+        }
+
+        public T Peek()
+        {
+            if (IsEmpty)
+            {
+                throw new InvalidOperationException("Empty");
+            }
+            T lastElement = _headNode.Element;
+            return lastElement;
+        }
+        public IEnumerator<T> GetEnumerator()
+        {
+            StackLinkedListNode<T> current = _headNode;
+            while (current != null)
+            {
+                yield return current.Element!;
+                current = current.Next;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
---
+++ DataStructures/Stacks/LinkedListBaseStack/StackLinkedListNode.cs
@@ -0,0 +1,20 @@
+﻿namespace DataStructuresAndAlgorithms.DataStructures.Stacks.LinkedListBaseStack
+{
+    public class StackLinkedListNode<T>
+    {
+        internal T? Element;
+        internal StackLinkedListNode<T>? Next { get; set; }
+
+        public StackLinkedListNode(T? element)
+        {
+            Element= element;
+            Next = null;
+        }
+        public StackLinkedListNode(T? element, StackLinkedListNode<T>? next)
+        {
+            Element = element;
+            Next = next;
+        }
+
+    }
+}
