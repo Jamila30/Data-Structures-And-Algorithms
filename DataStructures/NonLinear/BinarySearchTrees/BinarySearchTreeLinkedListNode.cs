c0b2c0fed799588a0d1262367e83151b4cdc572c
Jamila Ismayilzada <cemile.ismayilzade02@gmail.com>
2025-09-14 20:07:36 +0400

    BinarySearchTree class created and added IterativeInsert method

---
+++ DataStructures/NonLinear/BinarySearchTrees/BinarySearchTreeLinkedListNode.cs
@@ -0,0 +1,16 @@
+﻿namespace DataStructuresAndAlgorithms.DataStructures.NonLinear.BinarySearchTrees
+{
+    public class BinarySearchTreeLinkedListNode<T> where T : IComparable<T>
+    {
+        public T? Element;
+        public BinarySearchTreeLinkedListNode<T>? LeftChild ;
+        public BinarySearchTreeLinkedListNode<T>? RightChild ;
+        public BinarySearchTreeLinkedListNode(T item)
+        {
+            Element = item;
+            LeftChild = null;
+            RightChild = null;
+        }
+
+    }
+}
